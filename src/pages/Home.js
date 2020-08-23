0fbaf3367c664b33b7a699636681560167e00030
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-23 12:34:42 +0700

    Adding Link to home.js and styling with home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Home page</h1>
-
-            <div className="home__header">
-                <div className="home__headerLeft">
-
-                </div>
-                <div className="home__headerRight">
-                    
-                </div>
-            </div>
-            <div className="home__body">
-                 
-            </div>
-        </div>
-    )
+	return (
+		<div className="home">
+			<div className="home__header">
+				<div className="home__headerLeft">
+					<Link to="/about">About</Link>
+					<Link to="/store">Store</Link>
+				</div>
+				<div className="home__headerRight">
+					<Link to="/gmail">Gmail</Link>
+					<Link to="/images">Images</Link>
+				</div>
+			</div>
+			<div className="home__body" />
+		</div>
+	);
 }
 
-export default Home
+export default Home;
