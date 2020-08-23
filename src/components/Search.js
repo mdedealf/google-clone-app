213f7d8aea1320d37fedfd97bd9daa538cb6fe7b
Mdedealf <dede.alfaruqq@gmail.com>
2020-08-23 17:43:53 +0700

    add search components

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Search.css';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
+
+function Search() {
+	const [ input, setInput ] = useState('');
+
+	const search = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className="search">
+			<div className="search__input">
+				<SearchIcon className="search__inputIcon" />
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					placeholder="Search Google or type a URL"
+					type="text"
+				/>
+				<MicIcon />
+			</div>
+			<div className="search__buttons">
+				<Button onClick={search} variant="outlined">
+					Google Search
+				</Button>
+				<Button variant="outlined">I'm Feeling Lucky</Button>
+			</div>
+		</div>
+	);
+}
+
+export default Search;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+import Search from '../components/Search';
 
 function Home() {
 	return (
@@ -13,9 +16,14 @@ function Home() {
 				<div className="home__headerRight">
 					<Link to="/gmail">Gmail</Link>
 					<Link to="/images">Images</Link>
+					<AppsIcon />
+					<Avatar />
 				</div>
 			</div>
-			<div className="home__body" />
+			<div className="home__body">
+				<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+				<Search />
+			</div>
 		</div>
 	);
 }
